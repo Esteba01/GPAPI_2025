f84265c0c28cde7d592e84a3972aa7a4318f80db
jorge <jorge.perez01epn@gmail.com>
2025-03-21 11:40:42 -0500

    1.1: Functionality tested of GoogleAPI, implemented script to test API.

--- gpapi/run_gpapi.py
+++ gpapi/run_gpapi.py
@@ -1,7 +1,11 @@
 import sys
+import json
+
 sys.path.insert(0, "path/to/gpapi.zip")  # Replace with the actual path to the zip file
 
 from gpapi.googleplay import GooglePlayAPI
+from dotenv import load_dotenv
+import os
 
 try:
     # Initialize the API
@@ -13,8 +17,15 @@ except Exception as e:
 
 try:
     # Login to the API
-    email = "obsec.epn@gmail.com"
-    password = "obsecepn2024"
+    # Load environment variables from .env file
+    load_dotenv()
+
+    email = os.getenv("EMAIL_ACCOUNT")
+    password = os.getenv("APP_PASSWORD")
+
+    if not email or not password:
+        print("Error: Email or password not found in .env file")
+        sys.exit(1)
     api.login(email=email, password=password)
 except Exception as e:
     print(f"Error logging in: {e}")
@@ -22,7 +33,14 @@ except Exception as e:
 
 try:
     # Search for an app
-    results = api.search("example app")
+    search = str(input("Search for an app - APP NAME: "))
+    results = api.search(search)
+    # results = api.search("pinterest")
     print("Search Results:", results)
+
+    # Export results to a file
+    with open("search_results.json", "w", encoding="utf-8") as file:
+        json.dump(results, file, ensure_ascii=False, indent=4)
+    print("Results exported to 'search_results.json'")
 except Exception as e:
     print(f"Error searching for app: {e}")
